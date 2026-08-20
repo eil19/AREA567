e992cf6090f5987cd6a8da8544efc0cd14e1aa9b
unipug1 <adrivefans@gmail.com>
2026-08-20 16:46:37 +0800

    oops

--- WIU_AREA567/Assets/Scripts/Crafting System/CraftingSystem.cs
+++ WIU_AREA567/Assets/Scripts/Crafting System/CraftingSystem.cs
@@ -1,38 +1,38 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Events;
+//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.Events;
 
-public class CraftingSystem : MonoBehaviour
-{
-    public const int GRID_SIZE = 3;
-    public UnityEvent OnGridChanged;
+//public class CraftingSystem : MonoBehaviour
+//{
+//    public const int GRID_SIZE = 3;
+//    public UnityEvent OnGridChanged;
 
-    private List<CraftingRecipe> craftingRecipesList;
+//    private List<CraftingRecipe> craftingRecipesList;
 
-    // check crafting output
-    public Item CheckCraftingOutput(Item[] gridItems)
-    {
-        foreach (var recipe in craftingRecipesList)
-        {
-            if (IsMatchingRecipe(gridItems, recipe.recipeGrid))
-            {
-                return recipe.outputItem; // return output if recipe matches
-            }
-        }
-        return null; // no matching recipe
-    }
+//    // check crafting output
+//    public Item CheckCraftingOutput(Item[] gridItems)
+//    {
+//        foreach (var recipe in craftingRecipesList)
+//        {
+//            if (IsMatchingRecipe(gridItems, recipe.recipeGrid))
+//            {
+//                return recipe.outputItem; // return output if recipe matches
+//            }
+//        }
+//        return null; // no matching recipe
+//    }
 
-    private bool IsMatchingRecipe(Item[] grid, Item[] recipe)
-    {
-        for (int i = 0; i < grid.Length; i++)
-        {
-            if (grid[i] != recipe[i])
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-}
+//    private bool IsMatchingRecipe(Item[] grid, Item[] recipe)
+//    {
+//        for (int i = 0; i < grid.Length; i++)
+//        {
+//            if (grid[i] != recipe[i])
+//            {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
+//}
