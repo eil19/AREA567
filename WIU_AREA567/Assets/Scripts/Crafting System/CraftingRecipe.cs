bb40280d08cccd8a8357d651bb64833c287ef5b9
unipug1 <adrivefans@gmail.com>
2026-08-20 16:36:07 +0800

    minor changes to crafting

--- WIU_AREA567/Assets/Scripts/Crafting System/CraftingRecipe.cs
+++ WIU_AREA567/Assets/Scripts/Crafting System/CraftingRecipe.cs
@@ -3,6 +3,16 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "CraftingRecipe", menuName = "CraftingSystem/CraftingRecipe")]
 public class CraftingRecipe : ScriptableObject
 {
-    public Item outputItem; // result item of grid
-    public Item[] recipeGrid = new Item[9]; // 3x3 grid
+    [Header("Recipe Information")]
+    public string recipeName;
+
+    [TextArea]
+    public string recipeDescription;
+
+    [Header("Ingredients")]
+    public CraftingIngredient[] ingredients;
+
+    [Header("Output")]
+    public ItemData outputItem;
+    public int outputQuantity = 1;
 }
